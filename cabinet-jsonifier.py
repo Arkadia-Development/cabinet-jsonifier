1ff311bae2673abfb11f82fd8229bfea990af026
Deuce Black <56012328+VitaminTussle@users.noreply.github.com>
2022-01-19 16:41:49 -0600

    Added exclusion check for unsure cabinets

--- cabinet-jsonifier.py
+++ cabinet-jsonifier.py
@@ -4,28 +4,29 @@ json = "["
 
 with open("cabinet-info.txt", "r") as file:
 	for line in file:
-		json += "{\"id\":\""
+		if line[len(line) - 1] != "?":
+			json += "{\"id\":\""
 
-		paren = line.find("(")
-		title = line[0:paren]
-		title = "".join(c for c in title if c.isalnum())
-		title = title.lower()
-		json += (title + "\",\"isWorking\":")
+			paren = line.find("(")
+			title = line[0:paren]
+			title = "".join(c for c in title if c.isalnum())
+			title = title.lower()
+			json += (title + "\",\"isWorking\":")
 
-		if line[len(line) - 1] == "x":
-			json += "false"
-		else:
-			json += "true"
-		json += (",\"searchTerms\":[\"" + title + "\"")
+			if line[len(line) - 1] == "x":
+				json += "false"
+			else:
+				json += "true"
+			json += (",\"searchTerms\":[\"" + title + "\"")
 
-		closeParen = line.find(")")
-		if closeParen - paren > 1:
-			pub = line[(paren + 1):closeParen]
-			pubList = pub.split()
-			for p in pubList:
-				json += (",\"" + p.lower() + "\"")
-		
-		json += "]},"
+			closeParen = line.find(")")
+			if closeParen - paren > 1:
+				pub = line[(paren + 1):closeParen]
+				pubList = pub.split()
+				for p in pubList:
+					json += (",\"" + p.lower() + "\"")
+			
+			json += "]},"
 
 if json[len(json) - 1] == ",":
 	json = json[:-1]
