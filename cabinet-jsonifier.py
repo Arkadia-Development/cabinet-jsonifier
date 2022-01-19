d93da22718688d2443306f4c8211f9f8325001dc
Deuce Black <56012328+VitaminTussle@users.noreply.github.com>
2022-01-19 16:27:25 -0600

    Wrote initial version of script
    Also added raw data txt file and empty json file to write to

---
+++ cabinet-jsonifier.py
@@ -0,0 +1,33 @@
+import string
+
+lines = []
+
+json = "["
+
+with open("cabinet-info.txt", "r") as file:
+	for line in file:
+		if line != file[0]:
+			json += ","
+		json += "{\"id\":\""
+
+		paren = line.find("(")
+		title = line[0:paren]
+		title = filter(title.isalnum, string.printable).lower()
+		json += (title + "\",\"isWorking\":")
+
+		if line[len(line) - 1] == "x":
+			json += "false"
+		else:
+			json += "true"
+		json += (",\"searchTerms\":[\"" + title + "\"")
+
+		closeParen = line.find(")")
+		if closeParen - paren > 1:
+			pub = line[paren:(closeParen + 1)]
+			pubList = pub.split()
+			for p in pubList:
+				json += (",\"" + p + "\"")
+		
+		json += "]}"
+
+json += "]"
